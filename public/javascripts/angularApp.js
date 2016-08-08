78200c36458ac3be28004e9738d93a2488e94124
Suresh Sarda <sureshssarda@gmail.com>
2016-08-08 23:25:32 +0530

    stylesheet removed

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -2,6 +2,7 @@ var app = angular.module('ticketsapp', [
             'ui.router', 
             'ticketsapp.user',
             'ticketsapp.user.service',
+            'ticketsapp.user.controller',
             'ticketsapp.ticket',
             'ticketsapp.ticket.service',
             'ticketsapp.ticket.controller'
