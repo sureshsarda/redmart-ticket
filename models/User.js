4566a427780296a747c077a559fa87e772cdbaa9
Suresh Sarda <sureshssarda@gmail.com>
2016-08-13 19:25:47 +0530

    Fields added in user to deactivate and profile pic

--- models/User.js
+++ models/User.js
@@ -5,6 +5,8 @@ var Schema       = mongoose.Schema;
 var UserSchema = new Schema({
 	name: String,
 	email: {type: String, required: true, unique: true},
+	isActive: {type: Boolean, default: true},
+	image: {type: String, default: 'https://eliaslealblog.files.wordpress.com/2014/03/user-200.png'},
 	type: {
 		type: String,
 		enum: ['Customer', 'CSR'],
